5822dada647b0f18837eb6af5435cfffaf67eb3f
Ivan De La Cruz <ivandlacruzzz@gmail.com>
2025-08-08 20:11:13 -0700

    Renombrando temporalmente para resolver conflicto de mayúsculas

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Hero from './components/hero/Hero'
-import SelectorEstado from './components/Sucursales/SelectorEstado'
 import ContenedorSucursales from './components/Sucursales/ContenedorSucursales'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Promos } from './components/sections/promos/Promos'
+import BotonWhatsapp from './components/BotonWhatsapp/BotonWhastapp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hero/>
-    <ContenedorSucursales/>
     <Promos/>
+    <ContenedorSucursales/>
+    <BotonWhatsapp/>
   </StrictMode>,
 )
