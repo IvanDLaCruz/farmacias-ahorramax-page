3450164cba0dc921a9abe0d7366f045e885af0cb
Ivan De La Cruz <ivandlacruzzz@gmail.com>
2025-08-09 22:10:01 -0700

    euu push

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className="hero-content">
         <div className="hero-text">
           <h1>¡Mas barato que las del mono bailarin!</h1>
-          <p>Aprovecha nuestras ofertas! Encuentra una amplia variedad de medicamentos genéricos, similares y de patente. También contamos con productos naturales, artículos de ortopedia y todo lo necesario para rehabilitación.</p>
+          <p>¡Aprovecha nuestras ofertas! Encuentra una amplia variedad de medicamentos genéricos, similares y de patente. También contamos con productos naturales, artículos de ortopedia y todo lo necesario para rehabilitación.</p>
           <a href="#ofertas" className="hero-button">Ver promociones</a>
         </div>
         <div className="hero-image">
@@ -14,8 +14,7 @@ function Hero() {
             src="/images/promo-25.png"
             alt="Promoción del 25% de descuento"
             className="promo-img"
-
-                   />   
+            />   
         </div>
         
       </div>
