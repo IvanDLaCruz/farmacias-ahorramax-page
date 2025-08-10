96d17e586fdfc09137bcb3bfafd5691493d0fa2a
Ivan De La Cruz <ivandlacruzzz@gmail.com>
2025-08-09 19:42:55 -0700

    se agrego tipografia global

---
+++ src/components/experiencia-counter/ExperienciaCounter.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import "./experienciacounter.css";
+
+const ExperienciaCounter = ({ numeroFinal, duracion, texto }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let start = 0;
+    const stepTime = Math.abs(Math.floor(duracion / numeroFinal));
+
+    const timer = setInterval(() => {
+      start += 1;
+      setCount(start);
+      if (start >= numeroFinal) clearInterval(timer);
+    }, stepTime);
+
+    return () => clearInterval(timer);
+  }, [numeroFinal, duracion]);
+
+  return (
+    <div className="experiencia-counter">
+      <div className="numero-simbolo">
+        <span className="simbolo">+</span>
+        <span className="numero">{count}</span>
+      </div>
+      <p className="texto">{texto}</p>
+    </div>
+  );
+};
+
+export default ExperienciaCounter;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Promos } from './components/sections/promos/Promos'
 import BotonWhatsApp from './components/boton-whatsapp/BotonWhastapp';
-import { Footer } from './components/footer/footer';
+import './styles/tipografia.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +14,6 @@ createRoot(document.getElementById('root')).render(
     <Promos/>
     <ContenedorSucursales/>
     <BotonWhatsApp/>
-    <Footer/>
+
   </StrictMode>,
 )
