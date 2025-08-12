335efeaabf8486d95e292113839a46a36941c445
Ivan De La Cruz <ivandlacruzzz@gmail.com>
2025-08-11 20:32:19 -0700

    se agrego slider de categorias de producctos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import ContenedorSucursales from "../components/Sucursales/ContenedorSucursales"
 import BotonWhatsApp from "../components/boton-whatsapp/BotonWhastapp";
 import { Footer } from "../components/footer/Footer";
 import "../styles/tipografia.css";
+import CategoriasSlider from "../components/categorias-productos/CategoriasSlider";
+
 
 export const Home = () => {
   return (
@@ -11,6 +13,7 @@ export const Home = () => {
       <Hero />
       <Promos />
       <ContenedorSucursales />
+      <CategoriasSlider/>
       <BotonWhatsApp />
       <Footer />
     </>
