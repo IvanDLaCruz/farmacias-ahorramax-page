323d5e2ece5ed7ae4f9468cf4bee71240e419e24
Ivan De La Cruz <ivandlacruzzz@gmail.com>
2025-09-29 23:05:14 -0700

    se agrego reha

--- src/components/categorias-productos/categorias.js
+++ src/components/categorias-productos/categorias.js
@@ -3,7 +3,7 @@ const categorias = [
   { nombre: "Genéricos", icon: "/images/iconos/genericos.png" },
   { nombre: "Patentes", icon: "/images/iconos/patente.png" },
   { nombre: "Naturales", icon: "/images/iconos/naturales.png" },
-  { nombre: "Ortopedia", icon: "/images/iconos/ortopedia.png" },
+  { nombre: "Ortopedia y Rehabilitacion", icon: "/images/iconos/ortopedia.png" },
   { nombre: "Bienestar Sexual", icon:  "/images/iconos/salud-sexual.png" },
    { nombre: "Cuidado y prevencion", icon:  "/images/iconos/salud.png" },
 
